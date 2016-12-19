f19d188d4db5b7f8c95a2b8754ecfa8a03d3705f
Vactors <aknowleskp@gmail.com>
2016-12-19 15:40:42 -0500

    created metallicflakeshader class

---
+++ sunflow/src/org/sunflow/core/shader/MetallicFlakeShader.java
@@ -0,0 +1,29 @@
+package org.sunflow.core.shader;
+
+import org.sunflow.SunflowAPI;
+import org.sunflow.core.ParameterList;
+import org.sunflow.core.Shader;
+import org.sunflow.core.ShadingState;
+import org.sunflow.image.Color;
+
+public class MetallicFlakeShader implements Shader {
+
+	@Override
+	public boolean update(ParameterList pl, SunflowAPI api) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Color getRadiance(ShadingState state) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void scatterPhoton(ShadingState state, Color power) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
